d45cbda4175483059a759ba0b93127740d345fb8
redjajaggg <102412196+redjajaggg@users.noreply.github.com>
2023-07-22 15:50:41 +0700

    Update urls.py

--- Manhtia/urls.py
+++ Manhtia/urls.py
@@ -4,11 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("content/", views.content, name="content"),
-    path("content/<str:name>", views.each, name="each"),
+    path("content/<str:name>/", views.each, name="each"),
+    path("content/<str:name>/<str:chapter>", views.chapterviewer, name="chapterviewer"),
     path("portrayal", views.portrayal, name="portrayal"),
     path("manager", views.manager, name="manager"),
     path("private/", views.private, name="private"),
-    path("private/<str:name>", views.pri_each, name="mature_each"),
+    path("private/<str:name>/", views.pri_each, name="mature_each"),
+    path("private/<str:name>/<str:part>", views.pri_each_viewer, name="pri_each_viewer"),
     path("georgeous", views.georgeous, name="georgeous"),
     path("search", views.search, name="search")
 ]
