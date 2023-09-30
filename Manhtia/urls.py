c00b118656995358930e8204c3c90f62cba3c67c
redjajaggg <102412196+redjajaggg@users.noreply.github.com>
2023-09-30 19:58:38 +0700

    Update urls.py

--- Manhtia/urls.py
+++ Manhtia/urls.py
@@ -1,12 +1,17 @@
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.test import SimpleTestCase, override_settings
 from django.urls import *
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("content/", views.content, name="content"),
     path("content/<str:name>/", views.each, name="each"),
     path("content/<str:name>/<str:chapter>", views.chapterviewer, name="chapterviewer"),
-    path("portrayal", views.portrayal, name="portrayal"),
+    path("portrayal/", views.portrayal, name="portrayal"),
+    path("portrayal/<str:name>", views.portrayal_each, name="portrayalviwer"),
     path("manager", views.manager, name="manager"),
     path("private/", views.private, name="private"),
     path("private/<str:name>/", views.pri_each, name="mature_each"),
@@ -14,3 +19,4 @@ urlpatterns = [
     path("georgeous", views.georgeous, name="georgeous"),
     path("search", views.search, name="search")
 ]
+
